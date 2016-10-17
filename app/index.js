f5162717c7a99bdae4729f5a529822d99ca56b56
“Shaun <shaunwmusic@gmail.com>
2016-10-17 10:34:25 -0500

    Today's lecture

--- app/index.js
+++ app/index.js
@@ -3,7 +3,24 @@ import 'whatwg-fetch';
 // practicing with "prototypes"
 
 const duck = 'Dafty';
+const bunny = 'Bugs Bunny';
 
 console.log(duck.length);
 
 console.log(duck.split(''));
+
+// what if we didn't have "prototypes"
+
+const h = {
+  firstName: 'homer',
+  lastName: 'Simpson',
+  age: 39,
+  ocupation: 'safety inspector',
+};
+
+const m = {
+  firstName: 'marge',
+  lastName: 'Simpson',
+  age: 36,
+  ocupation: 'currently unemployed',
+};

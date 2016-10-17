1cc99db1bc6291293410e5b56cdf9fca02e919eb
“Shaun <shaunwmusic@gmail.com>
2016-10-17 10:58:23 -0500

    Using New keyword and constructor

--- app/index.js
+++ app/index.js
@@ -42,4 +42,20 @@ console.log(m2.getFullName);
 console.log(m2.getFullName());
 console.log(h2.getFullName);
 console.log(h2.getFullName());
+
+//the "new" keyword and constructor functions
+// document is lower case
+// string upper case
+//
+functions Simpson(firstName, lastName, age, occupation) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.age = age;
+  this.occupation = occupation;
+}
+
+//use this to refer to a current object
+
+const m3 = new Simpson('marge', 'simpson', 36, 'currently unemployed');
+
 debugger;

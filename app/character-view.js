da1abfe79776da033cf8d6e37ed14927213e35e5
“Shaun <shaunwmusic@gmail.com>
2016-10-17 15:05:17 -0500

    2:45 lecture on rendering and constructor

---
+++ app/character-view.js
@@ -0,0 +1,21 @@
+export default class CharacterView(data) {
+constructor(model) {
+  //with this pattern always start with this.
+  this.model = model;
+
+
+  this.el = document.createElement('div');
+  this.el.classList.add('character');
+  this.el.innerHTML = `
+  <p class="character__name"></p>
+  <p class="character__profession"></p>
+  <p class="character__age"></p>`;
+}
+//to fill in the element use a function called "render"
+  render() {
+//changed from a simple function.
+  this.el.querySelector('.character__name').innerText = `${this.model.first} ${this.model.last}`; 'Olivia Dunham';
+  this.el.querySelector('.character__profession').innerText = this.model.profession;
+  this.el.querySelector('.character__age').innerText = this.model.age;
+}
+}
